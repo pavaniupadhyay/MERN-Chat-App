4bb19049bee964de52ba91ad582eda4f4b246180
pavani upadhyay <pavaniupadhyay@gmail.com>
2024-07-31 07:29:08 -0700

    initiall commit

--- public/frontend/src/pages/Chat.jsx
+++ public/frontend/src/pages/Chat.jsx
@@ -4,10 +4,12 @@ import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import { allUsersRoute } from '../APIRoutes';
 import Contacts from '../components/Contacts';
-function Chat() {
-  const navigate=useNavigate();
-  const [contects,setcontects]=useState([]);
-  const [currentUser,setCurrentUser]=useState(undefined);
+export default function Chat() {
+  const navigate = useNavigate();
+  const socket = useRef();
+  const [contacts, setContacts] = useState([]);
+  const [currentChat, setCurrentChat] = useState(undefined);
+  const [currentUser, setCurrentUser] = useState(undefined);
   useEffect(async () => {
     if (!localStorage.getItem(process.env.REACT_APP_LOCALHOST_KEY)) {
       navigate("/login");
@@ -19,42 +21,59 @@ function Chat() {
       );
     }
   }, []);
-  useEffect(async()=>{
-if(currentUser){
-   if(currentUser.isAvatarImageSet){
-    const data=await axios.get(`${allUsersRoute}/${currentUser._id}`);
-    setcontects(data.data);
-    }else{
-      navigate("/setAvatar");
+  useEffect(() => {
+    if (currentUser) {
+      socket.current = io(host);
+      socket.current.emit("add-user", currentUser._id);
     }
-}
-  },[currentUser]);
-  return(
-<Container>
-    <div className="chat-container"></div>
-  <Contacts contects={contects} currentUser={currentUser}/>
-       </Container>
-  ); 
-}
-const Container=styled.div`
-width: 100vw;
-height: 100vh;
-display: flex;
-flex-direction: column;
-justify-content: center;
-align-items: center;
-gap: 1rem;
-.container{
-  width: 85vw;
-  height: 85vh;
-  background-color: #00000076;
-  display: grid;
-  grid-template-columns: 25% 75%;
-  @media screen and(min-width: 720px) and (max-width:1080px){
-    grid-template-columns: 30% 65%;
-    
-  }
-}
-`;
-export default Chat;
+  }, [currentUser]);
 
+  useEffect(async () => {
+    if (currentUser) {
+      if (currentUser.isAvatarImageSet) {
+        const data = await axios.get(`${allUsersRoute}/${currentUser._id}`);
+        setContacts(data.data);
+      } else {
+        navigate("/setAvatar");
+      }
+    }
+  }, [currentUser]);
+  const handleChatChange = (chat) => {
+    setCurrentChat(chat);
+  };
+  return (
+    <>
+      <Container>
+        <div className="container">
+          <Contacts contacts={contacts} changeChat={handleChatChange} />
+          {currentChat === undefined ? (
+            <Welcome />
+          ) : (
+            <ChatContainer currentChat={currentChat} socket={socket} />
+          )}
+        </div>
+      </Container>
+    </>
+  );
+}
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 1rem;
+  align-items: center;
+  background-color: #131324;
+  .container {
+    height: 85vh;
+    width: 85vw;
+    background-color: #00000076;
+    display: grid;
+    grid-template-columns: 25% 75%;
+    @media screen and (min-width: 720px) and (max-width: 1080px) {
+      grid-template-columns: 35% 65%;
+    }
+  }
+`;

4e386fa5457357b1e287603e54b4a4b77155f1b6
pavani upadhyay <pavaniupadhyay@gmail.com>
2024-08-10 00:31:41 -0700

    inital commit

--- server/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const { register, login } = require("../controlers/userController");
-
-const router=require("express").Router();
-router.post("/register",register);
-router.post("/login",login);
-router.post("/setAvatar/:id",setAvatar);
-router.get("/allUsers/:id",getAllUsers);
-
-module.exports=router;
